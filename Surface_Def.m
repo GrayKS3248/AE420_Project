bcef567ca62834eea672e1f45f804eadb1fd44dc
Grayson Schaer <gschaer2@illinois.edu>
2021-11-11 19:00:12 -0600

    Geometry without threads

--- Surface_Def.m
+++ Surface_Def.m
@@ -2,23 +2,32 @@
 close all 
 clear all
 
+
 %% Parameters
+% Radii
+r_head = 6.0;
+r_shank = 2.5;
+r_body = 2.0;
 
-r_head = 0.0;
-r_shank = 0.0;
-r_body = 0.0;
+% Lengths
+l_head_taper = 1.0;
+l_head = 4.0;
+l_shank_transition = 1.0;
+l_shank = 4.0;
+l_body = 40.0;
+l_tip = 5.0;
 
-l_head_taper = 0.0;
-l_head = 0.0;
-l_shank_transition = 0.0;
-l_shank = 0.0;
-l_body = 0.0;
-l_tip = 0.0;
-
+% Thread
 thread_number = 0.0;
 thread_depth = 0.0;
 thread_width = 0.0;
 thread_crest = 0.0;
+
+% Mesh
+num_height = 20;
+num_ring = 2;
+num_theta = 10;
+
 
 %% Compile parameters
 arg = struct('rh',r_head,...
@@ -34,3 +43,111 @@ arg = struct('rh',r_head,...
     'td',thread_depth,...
     'tw',thread_width,...
     'tc',thread_crest);
+
+
+%% Generate nodes
+% Calculate general parameters
+total_length = arg.lt + arg.lb + arg.ls + arg.lst + arg.lh + arg.lht;
+num_nodes = num_height*num_ring*num_theta;
+
+% Calculate height, ring, and theta space
+height_list = linspace(0.0,total_length,num_height);
+radius_list = linspace(1.0,0.0,num_ring+2);
+theta_list = linspace(0.0,2.0*pi,num_theta+1);
+theta_list = theta_list(1:end-1);
+
+X = zeros(num_nodes, 1);
+Y = zeros(num_nodes, 1);
+Z = zeros(num_nodes, 1);
+TYPE = zeros(num_nodes, 1);
+node = 1;
+for height = height_list
+    for radius = radius_list
+        for theta = theta_list
+            
+            % Get radius, convert to cartesian
+            [r, node_type] = get_r(theta,height,arg);
+            r = r * radius;
+            [x,y,z] = pol2cart(theta, r, height);
+            X(node) = x;
+            Y(node) = y;
+            Z(node) = z;
+            
+            % Assign type to surface nodes only
+            if radius == 1.0
+                TYPE(node) = node_type;
+            else
+                TYPE(node) = -1;
+            end
+            node = node + 1;
+            
+        end
+    end
+end
+nodes = [X Y Z];
+[nodes, ia, ic] = unique(nodes,'rows','stable');
+
+
+%% Visualization
+scatter3(X,Y,Z,'.','k');
+axis equal;
+view(30,15);
+
+%% Define body geometry
+function [r, node_type] = get_r(theta,h,arg)
+    
+    % Calculate relevant heights
+    tip_2_body = arg.lt;
+    body_2_shank = tip_2_body + arg.lb;
+    shank_2_shank_transition = body_2_shank + arg.ls;
+    shank_transition_2_head = shank_2_shank_transition + arg.lst;
+    head_2_head_taper = shank_transition_2_head + arg.lh;
+    total_length = head_2_head_taper + arg.lht;
+
+    % Head taper
+    if h>=head_2_head_taper && h<=total_length
+        node_type = 8;
+        loc_in_section = (h - head_2_head_taper) / (total_length - head_2_head_taper);
+        r = arg.rh*(1.0-loc_in_section);
+        
+    % Head
+    elseif h>=shank_transition_2_head && h<=head_2_head_taper
+        node_type = 7;
+        r = rh;
+        
+    % Shank transition
+    elseif h>=shank_2_shank_transition && h<=shank_transition_2_head
+        node_type = 6;
+        loc_in_section = (h - shank_2_shank_transition) / (shank_transition_2_head - shank_2_shank_transition);
+        r = arg.rs*(1.0-loc_in_section) + arg.rh*loc_in_section;
+        
+    % Shank
+    elseif h>=body_2_shank && h<=shank_2_shank_transition
+        node_type = 5;
+        r = arg.rs;
+        
+    % Body
+    elseif h>=tip_2_body && h<=body_2_shank
+        [thread_height, node_type] = get_thread_height(theta,h,arg);
+        r = arg.rb + thread_height;
+        
+    % Tip
+    elseif h>=0.0 && h<=tip_2_body
+        [thread_height, node_type] = get_thread_height(theta,h,arg);
+        loc_in_section = h/tip_2_body;
+        r = (arg.rb+thread_height)*loc_in_section;
+        
+    % Outside of body
+    else
+        node_type = -1;
+        r = 0.0;
+        
+    end
+    
+end
+
+%% Define thread geometry
+function [thread_height, node_type] = get_thread_height(theta,h,arg)
+    thread_height = 0.0;
+    node_type = 0;
+end
